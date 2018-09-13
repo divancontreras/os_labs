dccc60b5d79c8874e6da86443b36adc8d6b78870
Diego Ivan <divancontreras@hotmail.com>
2018-09-13 15:40:21 -0600

    Initing lab1.9

---
+++ lab1.9/multiplier.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+
+int main()
+
+long * readMatrix(char *filename)
+
+long * getColumn(int col, long *matrix)
+
+long * getRow(int row, long *matrix)
+
+int getLock()
+
+int releaseLock(int lock)
+
+
+long dotProduct(long *vec1, long *vec2){
+    int i, j;
+    long temp=0;
+    for (i = 0; sizeof(vec1); i++){
+        temp += 
+    }
+}
+
+long * multiply(long *matA, long *matB)
+
+int saveResultMatrix(long *result)
